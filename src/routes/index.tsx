92abdd57b1f1a8cc32ac31c32192768c449ac548
LucasLdp <lucasdepauloldp1@gmail.com>
2026-08-03 18:24:33 -0300

    feat: route tanstack setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,31 @@
+import { siteConfig } from "#/constants/site";
+import { HomePage } from "#/pages/home-page";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+	head: () => ({
+		title: `${siteConfig.name} | ${siteConfig.role}`,
+		meta: [
+			{ name: "description", content: siteConfig.description },
+			{
+				property: "og:title",
+				content: `${siteConfig.name} | ${siteConfig.role}`,
+			},
+			{ property: "og:description", content: siteConfig.description },
+			{ property: "og:type", content: "website" },
+			{ property: "og:url", content: siteConfig.siteUrl },
+			{ name: "twitter:card", content: "summary_large_image" },
+			{
+				name: "twitter:title",
+				content: `${siteConfig.name} | ${siteConfig.role}`,
+			},
+			{ name: "twitter:description", content: siteConfig.description },
+		],
+		links: [{ rel: "canonical", href: siteConfig.siteUrl }],
+	}),
+	component: Home,
+});
+
+function Home() {
+	return <HomePage />;
+}
